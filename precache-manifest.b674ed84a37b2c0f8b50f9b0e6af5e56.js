cb90975167d4526c9908cf09f1289a7915b81b83
chriswongatcuhk <wc38.wong@gmail.com>
2019-01-23 07:03:37 +0800

    Updates

---
+++ precache-manifest.b674ed84a37b2c0f8b50f9b0e6af5e56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "da71729cceda5fb627d7",
+    "url": "/React-CRUD-Tutorial/static/css/main.e22602fb.chunk.css"
+  },
+  {
+    "revision": "da71729cceda5fb627d7",
+    "url": "/React-CRUD-Tutorial/static/js/main.da71729c.chunk.js"
+  },
+  {
+    "revision": "a85981336badf7df1319",
+    "url": "/React-CRUD-Tutorial/static/js/1.a8598133.chunk.js"
+  },
+  {
+    "revision": "de4166236bfd8e1c066f",
+    "url": "/React-CRUD-Tutorial/static/js/runtime~main.de416623.js"
+  },
+  {
+    "revision": "2d9f64bbccba3e96fc7ddabc86668495",
+    "url": "/React-CRUD-Tutorial/static/media/john.2d9f64bb.png"
+  },
+  {
+    "revision": "7d951de63c70f0fcd5c9d1eb4db00bbf",
+    "url": "/React-CRUD-Tutorial/static/media/mark.7d951de6.png"
+  },
+  {
+    "revision": "6a8df8e02b0cb65dec71e8cea1c65fba",
+    "url": "/React-CRUD-Tutorial/static/media/mary.6a8df8e0.png"
+  },
+  {
+    "revision": "8809474f93c59825ca14c96f13e1c311",
+    "url": "/React-CRUD-Tutorial/index.html"
+  }
+];
