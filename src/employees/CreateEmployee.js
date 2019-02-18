541fa40cfe73962e4378b5c80a834daebe0f0174
Chris Wong <wc38.wong.public@gmail.com>
2019-02-18 15:14:56 +0800

    React radio button Add contact preference

--- src/employees/CreateEmployee.js
+++ src/employees/CreateEmployee.js
@@ -7,18 +7,28 @@ class CreateEmployee extends Component {
             { 
                 fullName: '',
                 email: '',
-                phoneNumber:''
+                phoneNumber:'',
+                contactPreference: '',
+                gender: ''
             } 
         };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange      = this.handleChange.bind(this);
+        this.handleRadioChange = this.handleRadioChange.bind(this);
+        this.handleSubmit      = this.handleSubmit.bind(this);
     }
 
     // handle the input change
     handleChange(event) {
         let employee = this.state.employee;
         employee[event.target.id] = event.target.value;
+        this.setState({employee: employee});
+    }
+
+    // handle the radio button change
+    handleRadioChange(event) {
+        let employee = this.state.employee;
+        employee[event.target.name] = event.target.value;
         this.setState({employee: employee});
     }
     
@@ -53,6 +63,20 @@ class CreateEmployee extends Component {
                                 <label htmlFor="phoneNumber">Phone Number</label>
                                 <input id="phoneNumber" type="text" className="form-control" name="phoneNumber" 
                                     value={this.state.employee.phoneNumber} onChange={this.handleChange}/>
+                            </div>
+
+                            <div className="form-group">
+                                <label>Contact Preference</label>
+                                <div className="form-control">
+                                <label className="radio-inline">
+                                    <input type="radio" name="contactPreference" value="email" onChange={this.handleRadioChange}/>
+                                    Email
+                                </label>
+                                <label className="radio-inline">
+                                    <input type="radio" name="contactPreference" value="phone" onChange={this.handleRadioChange}/>
+                                    Phone
+                                </label>
+                                </div>
                             </div>
 
                             <div className="panel-footer">
