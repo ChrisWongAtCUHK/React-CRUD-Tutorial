f780b507b9fd4e3a9cad90b4bb3c6c2e67b14c3d
chriswongatcuhk <wc38.wong@gmail.com>
2019-03-04 13:16:01 +0800

    Just a form

--- src/employees/CreateEmployee.js
+++ src/employees/CreateEmployee.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react';
 class CreateEmployee extends Component {
     render() {
         return (
-            <div>CreateEmployee works!</div>
+            <div>
+            <form onSubmit={this.handleSubmit}>
+                <div className="panel panel-primary">
+                <div className="panel-heading">
+                    <h3 className="panel-title">Create Employee</h3>
+                </div>
+                <div className="panel-body" />
+                <div className="panel-footer">
+                    <button className="btn btn-primary" type="submit">Save</button>
+                </div>
+                </div>
+            </form>
+            </div>
         )
     }
 }
