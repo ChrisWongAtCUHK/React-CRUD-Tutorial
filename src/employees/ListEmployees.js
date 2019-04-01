13270a5e2512df6d6c659a2550ef40985a59081a
chriswongatcuhk <wc38.wong@gmail.com>
2019-04-01 15:28:29 +0800

    View next Employee

--- src/employees/ListEmployees.js
+++ src/employees/ListEmployees.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import DisplayEmployee from './DisplayEmployee';
 
 class ListEmployees extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employeeToDisplayId: 0
+    };
+    this.nextEmployee = this.nextEmployee.bind(this);
+  }
+
+  nextEmployee(event) {
+    if (this.state.employeeToDisplayId < this.props.employees.length - 1) {
+      this.setState({employeeToDisplayId: this.state.employeeToDisplayId + 1});
+    } else {
+      this.setState({employeeToDisplayId: 0});
+    }
+  }
+
   render() {
     return (
       <div>
-        {this.props.employees.map(employee =>(  
-          <div>
-            <DisplayEmployee employee={employee}/>
-          </div>
-        ))}
+        <button onClick={this.nextEmployee} class="btn btn-primary">
+          View Next Employee
+        </button>
+        <br/><br/>
+        <DisplayEmployee employee={this.props.employees[this.state.employeeToDisplayId]} />
       </div>
     );
   }
