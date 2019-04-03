0f649b6722e70a1424be8bac86f9966b8f17c5ce
chriswongatcuhk <wc38.wong@gmail.com>
2019-04-03 15:41:15 +0800

    Implement the search input

--- src/employees/ListEmployees.js
+++ src/employees/ListEmployees.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import DisplayEmployee from './DisplayEmployee';
 
+const searchStyle = {
+  width: "300px"
+}
+
 class ListEmployees extends Component {
   constructor(props){
     super(props);
 
     this.state  = {
+      employees: this.props.employees,
       dataFromChild: null
     };
+  }
+
+  // filter by the search term
+  handleSearch(event){
+    let employees = this.props.employees.filter(employee =>
+      employee.name.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1);
+    this.setState({employees: employees});
   }
 
   // tricky parameter
@@ -21,7 +33,11 @@ class ListEmployees extends Component {
   render() {
     return (
       <div>
-        {this.props.employees.map(employee =>(  
+        <div className="form-group">
+            <input type="text" className="form-control" placeholder="Search By Name"
+                    style={searchStyle} onChange={this.handleSearch.bind(this)}/>
+        </div>
+        {this.state.employees.map(employee =>(  
             <DisplayEmployee key={employee.id} employee={employee} onClick={this.handleClick.bind(this, employee)} selectedEmployeeId={this.props.match.params.id} />
         ))}
       </div>
